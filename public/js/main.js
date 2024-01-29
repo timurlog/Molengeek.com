e8237ad2d98caa69d80ed808d507b225b179f459
timurlog <timur.logie@gmail.com>
2024-01-29 16:36:20 +0100

    added header, init

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,15 @@
 import data from '../json/molengeek.json' assert { type: "json" };
 
+// // SHOW MENU
+// const showMenu = (toggleId, navId) => {
+//     const toggle = document.getElementById(toggleId),
+//           nav = document.getElementById(navId)
+ 
+//     toggle.addEventListener('click', () => {
+//         nav.classList.toggle('show-menu')
+
+//         toggle.classList.toggle('show-icon')
+//     })
+// }
+
+// showMenu('navToggle','navMenu')
