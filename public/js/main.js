2ddcf005504efe28e313f822bff3a06fc2f8fddd
timurlog <timur.logie@gmail.com>
2024-01-30 23:40:43 +0100

    added partners section => finished

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,35 @@ const showMenu = (toggleId, navClass) => {
 }
 
 showMenu('navToggle','.nav-list')
+
+let partners = document.querySelector('.logos-slide').querySelectorAll('img')
+let partners2 = document.querySelectorAll('.logos-slide')[1].querySelectorAll('img')
+let partnersSrc = []
+let partnersSrc2 = []
+
+partners.forEach(partner => {
+    partnersSrc.push(partner)
+});
+
+partners2.forEach(partner => {
+    partnersSrc2.push(partner)
+});
+
+let partnersSrcArray = []
+const partenaires = data.partenaires;
+
+for (const partnerKey in partenaires) {
+    if (partenaires.hasOwnProperty(partnerKey)) {
+        const partner = partenaires[partnerKey];
+        const logoUrl = partner.logo;
+        partnersSrcArray.push(logoUrl)
+    }
+}
+
+for (let i = 0; i < partnersSrc.length; i++) {
+    partnersSrc[i].setAttribute('src', partnersSrcArray[i]);
+}
+
+for (let i = 0; i < partnersSrc.length; i++) {
+    partnersSrc2[i].setAttribute('src', partnersSrcArray[i]);
+}
