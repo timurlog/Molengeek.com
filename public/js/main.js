279350018d5aaf059aa673c38a7dfaab74abe1ad
timurlog <timur.logie@gmail.com>
2024-01-30 13:47:55 +0100

    added new header, added pages

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,15 @@
 import data from '../json/molengeek.json' assert { type: "json" };
 
-// // SHOW MENU
-// const showMenu = (toggleId, navId) => {
-//     const toggle = document.getElementById(toggleId),
-//           nav = document.getElementById(navId)
+// SHOW MENU
+const showMenu = (toggleId, navClass) => {
+    const toggle = document.getElementById(toggleId),
+          nav = document.querySelector(navClass)
  
-//     toggle.addEventListener('click', () => {
-//         nav.classList.toggle('show-menu')
+    toggle.addEventListener('click', () => {
+        nav.classList.toggle('show-menu')
 
-//         toggle.classList.toggle('show-icon')
-//     })
-// }
+        toggle.classList.toggle('show-icon')
+    })
+}
 
-// showMenu('navToggle','navMenu')
+showMenu('navToggle','.nav-list')
